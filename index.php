c598005ad359fab00d315018b90483f44c98aad2
ilgenuine <infx@live.se>
2018-04-24 16:17:47 +0700

    Footer

--- index.php
+++ index.php
@@ -9,6 +9,65 @@
     <?php include_once('inc/navbar.php'); ?>
 
 
+    <div class="container custom-container">
+
+        <a href="#"><font class="text-grey">ใบสั่งซื้อ ></a></font> <font class="text-darkblue">TEST TEXT</font>
+
+        <br>
+        <br>
+
+        <font class="title">ใบสั่งซื้อ</font>
+
+        <br>
+        <br>
+
+        <select class="custom-option">
+          <option>test1</option>
+          <option>test2</option>
+          <option>test3</option>
+          <option>test4</option>
+        </select>
+
+        <br>
+        <br>
+
+
+        <button  type="submit" class="btn btn-default btn-primary">
+          btn-primary
+        </button>
+
+        <button  type="submit" class="btn btn-default btn-ghost">
+          btn-ghost
+        </button>
+
+        <button  type="submit" class="btn btn-default btn-quantity">
+              <i class="glyphicon glyphicon-plus" aria-hidden="true"></i>
+        </button>
+
+        <button  type="submit" class="btn btn-default btn-quantity">
+              <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+        </button>
+
+
+        <br>
+        <br>
+
+        <font class="text-blue">test text</font>
+        /
+        <font class="text-darkblue">test text</font>
+        /
+        <font class="text-grey">test text</font>
+        /
+        <font class="text-red">test text</font>
+
+        <br>
+        <br>
+
+    </div>
+
+
+
+
 
     <?php include_once('inc/footer.php'); ?>
 
