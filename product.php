c102747cbdc2d84b46d932b3a11aa0d2e4c3b3b5
ilgenuine <infx@live.se>
2018-04-26 16:32:16 +0700

    productdetail.php

--- product.php
+++ product.php
@@ -585,18 +585,18 @@
               <nav aria-label="Page navigation">
                 <ul class="pagination">
                   <li>
-                    <a class="previous" href="#" aria-label="Previous">
-                      <span aria-hidden="true">&laquo;</span>
+                    <a class="inbox-pagination inbox-border-none pagination-right-set" href="#">
+                      <span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span>
                     </a>
                   </li>
-                  <li><a href="#">1</a></li>
-                  <li><a href="#">2</a></li>
-                  <li><a href="#">3</a></li>
-                  <li><a href="#">4</a></li>
-                  <li><a href="#">5</a></li>
+                  <li><a class="inbox-pagination" href="#">1</a></li>
+                  <li><a class="inbox-pagination" href="#">2</a></li>
+                  <li><a class="inbox-pagination" href="#">3</a></li>
+                  <li><a class="inbox-pagination" href="#">4</a></li>
+                  <li><a class="inbox-pagination" href="#">5</a></li>
                   <li>
-                    <a class="next" href="#" aria-label="Next">
-                      <span aria-hidden="true">&raquo;</span>
+                    <a class="inbox-pagination inbox-border-none pagination-right-set" href="#" >
+                      <span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>
                     </a>
                   </li>
                 </ul>
