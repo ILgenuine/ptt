572ef43050ad53842ba652fc7e19cceeb8a4e0ee
haritleksopee <33746030+haritleksopee@users.noreply.github.com>
2018-04-26 15:23:44 +0700

    add inbox.php
    time 15.23
    
    date 26/04/2561

---
+++ inbox.php
@@ -0,0 +1,187 @@
+<html lang="en">
+  <head>
+    <?php include_once('inc/header.php'); ?>
+  </head>
+
+  <body>
+
+    <?php include_once('inc/navbar.php'); ?>
+
+    <div class="col-12">
+
+      <div class="header-img-frame" style="height:400px;">
+        <div style="background: url(assets/images/bg/header-bg.jpg) fixed;z-index:-1;position:absolute;top:0;height:400px;width:100%;">
+        </div>
+          <!-- <img src="assets/images/bg/header-bg.jpg" class="img-responsive" style="width:100%;height:450px;z-index:-1;"/> -->
+
+          <div class="redtext">
+                <h1 class="redtext-title text-center pagination-centered">SP PTTGC</h1>
+          </div>
+
+          <div class="redtext-2 redtext-underline">
+          </div>
+
+          <div class="whitetext">
+            <div class="section-content">
+              <h3 class="redtext-title text-center pagination-centered">PARTNERSHIP OF CONSUMABLE CONTRACT</h3>
+            </div>
+          </div>
+      </div>
+    </div>
+<!-- ===================================== CONTENT ==================================== -->
+      <div class="row inbox-set">
+        <div class="col-sm-8">
+          <label class="text-darkblue inbox-set-lable">Inbox</label>
+        </div>
+        <div class="col-sm-4">
+            <input placeholder="ค้นหา" type="text" class="form-control search-form">
+            <button class="search-box">
+              <span class="glyphicon glyphicon-search form-control-feedback right-15"></span>
+            </button>
+        </div>
+        <div class="col-sm-12 mt-4">
+          <table class="width100 table">
+            <tr>
+              <th class="text-center" style="width: 5%;">No.</th>
+              <th class="text-center" style="width: 47%;">Topic</th>
+              <th class="text-center" style="width: 10%;">Message From</th>
+              <th class="text-center" style="width: 13%;">Message Date ▼</th>
+              <th class="text-center" style="width: 10%;">Message Reply</th>
+              <th class="text-center" style="width: 10%;">Mesage Date ▼</th>
+              <th class="text-center" style="width: 5%;"></th>
+            </tr>
+
+            <tr>
+              <td class="text-center text-grey">1</td>
+              <td class="text-darkblue">หัวข้อคำถาม AA</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+            <tr>
+              <td class="text-center text-grey">2</td>
+              <td class="text-darkblue">หัวข้อคำถาม BB</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center text-grey"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+            <tr>
+              <td class="text-center text-grey">3</td>
+              <td class="text-darkblue">หัวข้อคำถาม CC</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center text-grey"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+            <tr>
+              <td class="text-center text-grey">4</td>
+              <td class="text-darkblue">หัวข้อคำถาม DD</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center text-grey"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+            <tr>
+              <td class="text-center text-grey">5</td>
+              <td class="text-darkblue">หัวข้อคำถาม EE</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center text-grey"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+            <tr>
+              <td class="text-center text-grey">6</td>
+              <td class="text-darkblue">หัวข้อคำถาม FF</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center text-grey"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+            <tr>
+              <td class="text-center text-grey">7</td>
+              <td class="text-darkblue">หัวข้อคำถาม GG</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center text-grey"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+            <tr>
+              <td class="text-center text-grey">8</td>
+              <td class="text-darkblue">หัวข้อคำถาม HH</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center text-grey"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+            <tr>
+              <td class="text-center text-grey">9</td>
+              <td class="text-darkblue">หัวข้อคำถาม II</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center text-grey"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+            <tr>
+              <td class="text-center text-grey">10</td>
+              <td class="text-darkblue">หัวข้อคำถาม JJ</td>
+              <td class="text-center text-grey">มานะ ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 09.00</td>
+              <td class="text-center text-grey">มานี ใจดี</td>
+              <td class="text-center text-grey">01/01/2561 10.00</td>
+              <td class="text-center text-grey"><a class="glyphicon glyphicon-share inbox-icon-share"></a></td>
+            </tr>
+          </table>
+        </div>
+
+
+        <div class="col-sm-12 mt-4">
+          <div class="text-center">
+            <ul class="pagination">
+              <li>
+                <a class="inbox-pagination inbox-border-none pagination-right-set" href="#">
+                  <span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span>
+                </a>
+              </li>
+              <li><a class="inbox-pagination" href="#">1</a></li>
+              <li><a class="inbox-pagination" href="#">2</a></li>
+              <li><a class="inbox-pagination" href="#">3</a></li>
+              <li><a class="inbox-pagination" href="#">4</a></li>
+              <li><a class="inbox-pagination" href="#">5</a></li>
+              <li>
+                <a class="inbox-pagination inbox-border-none pagination-right-set" href="#" >
+                  <span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+    <?php include_once('inc/footer.php'); ?>
+
+  </body>
+
+
+
+</html>
+
+
+
+
+<?php include_once('inc/footer-script.php'); ?>
